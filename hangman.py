7862651e96ac24b59a1964b806608d92945e5a1b
saheed cp <saheed887@gmail.com>
2016-10-27 22:27:54 -0700

    implemented hide_word function successfully

--- hangman.py
+++ hangman.py
@@ -3,6 +3,10 @@ def secret_word():
     with open("/usr/share/dict/words") as f:
         for i in f:
             i=i.strip()
-            if i.isalpha() and len(i)==4:
+            if i.isalpha() and len(i)>5:
                 return i
 
+def hide_word():
+  word=secret_word()  
+  blanks="*"*len(word)
+  return blanks
