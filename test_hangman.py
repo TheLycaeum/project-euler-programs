05f985252935c242737f6229fb63b95a9371d931
saheed cp <saheed887@gmail.com>
2016-10-27 22:25:59 -0700

    test fails due to assertion error

--- test_hangman.py
+++ test_hangman.py
@@ -1,6 +1,10 @@
-from hangman import secret_word
+from hangman import secret_word,hide_word
 def test_secret_word():
    word = secret_word()
    assert(word != None)
-   assert(len(word)==4)
+   assert(len(word)>5)
 
+def test_hide_word():
+   word=secret_word()
+   blank_word=hide_word()
+   assert(len(word)==len(blank_word))
