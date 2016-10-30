cac28d73c76f7500a285726ea48053d423385111
saheed cp <saheed887@gmail.com>
2016-10-29 22:05:22 -0700

    function implemented successfully

--- hangman.py
+++ hangman.py
@@ -1,12 +1,16 @@
 import random
 def secret_word():
+    good_words=[]
     with open("/usr/share/dict/words") as f:
         for i in f:
             i=i.strip()
             if i.isalpha() and len(i)>5:
-                return i
-
-def hide_word():
-  word=secret_word()  
-  blanks="*"*len(word)
-  return blanks
+                good_words.append(i)
+    word=random.choice(good_words)
+    
+    return word
+def hide_word(word):
+    word="saheed"
+    blanks="*"*len(word)
+    print("word:" ,blanks)
+    return blanks
