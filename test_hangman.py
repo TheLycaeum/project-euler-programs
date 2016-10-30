e4ba268c6ff11c0494e327712b6fc616cfd89f19
saheed cp <saheed887@gmail.com>
2016-10-29 22:03:04 -0700

    failed due to implementing error

--- test_hangman.py
+++ test_hangman.py
@@ -1,10 +1,11 @@
-from hangman import secret_word,hide_word
+from hangman import secret_word
 def test_secret_word():
    word = secret_word()
    assert(word != None)
    assert(len(word)>5)
+  
 
 def test_hide_word():
-   word=secret_word()
-   blank_word=hide_word()
+   word="saheed"
+   blank_word=hide_word(word)
    assert(len(word)==len(blank_word))
