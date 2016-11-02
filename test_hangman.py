929b9e3691b97729938e7992a2fc4a7d1795b516
saheed cp <saheed887@gmail.com>
2016-11-02 09:40:55 -0700

    Test failed by importing error

--- test_hangman.py
+++ test_hangman.py
@@ -1,7 +1,8 @@
-from hangman import secret_word,hide_word
+from hangman import secret_word,hide_word,replace_blanks
 def test_secret_word():
+   f=open("/usr/share/dict/words")
    word = secret_word()
-   assert(word != None)
+   assert(word,f)
    assert(len(word)>5)
   
 
@@ -9,3 +10,10 @@ def test_hide_word():
    word="saheed"
    blank_word = hide_word(word)
    assert('******' == blank_word)
+
+def test_replace_blanks():
+   word="saheed"
+   guessed="e"
+   new=replace_blanks(word,guessed)
+   assert(word,guessed)
+   assert(new,guessed)
