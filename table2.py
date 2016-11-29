fd9981dea317ad052efdbfa0e8789d74dc512396
saheed cp <saheed887@gmail.com>
2016-11-29 10:01:35 -0800

    imlemented the program

---
+++ table2.py
@@ -0,0 +1,12 @@
+def sq_tables(n):
+    for i in range(1,n+1):
+        print(i,end="\t")    
+    print("")
+    print("-----"*7)
+    for j in range(2,n+1):
+        for k in range(1,n+1):
+            print(j*k,end="| \t")
+        
+        print("\n")
+if __name__=="__main__":
+    sq_tables(5)
