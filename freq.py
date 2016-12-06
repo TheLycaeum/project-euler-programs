98395338c2d64a60ab1b7909528b244999ca7d94
saheed cp <saheed887@gmail.com>
2016-12-06 08:58:25 -0800

    Removed unwanted lines

--- freq.py
+++ freq.py
@@ -1,12 +1,5 @@
 def freq(s):
     dict={}
- #   for i in s:
-  #      dict[i]=0
-   # for i in s:
-    #    dict[i]+=1
-    #print(dict)
-    #for i in "abcdefghijklmnopqrstuvwxyz":
-        
     for j in set(s):
         x=s.count(j,0,len(s))
         dict[j]=x
