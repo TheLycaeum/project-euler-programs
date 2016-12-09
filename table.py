2249bdedd2f40ad010f24389763a9f29bc36c6ba
saheed cp <saheed887@gmail.com>
2016-12-09 09:44:54 -0800

    included i*m in .format

--- table.py
+++ table.py
@@ -1,7 +1,7 @@
 def tables(m,n):
     for i in range (1,n+1):
-        out=i*m
-        print("{}  x  {} = ".format(i,m),out)
+       
+        print("{}  x  {} = ".format(i,m),"{}".format(i*m))
     
 if __name__=="__main__":
     tables(5,10)
