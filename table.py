5b0ae49023bfbd9f1c1702716e224ee022a0ac5b
Noufal Ibrahim <noufal@nibrahim.net.in>
2016-12-10 11:21:42 +0530

    Removes two args to print.

--- table.py
+++ table.py
@@ -1,7 +1,7 @@
 def tables(m,n):
     for i in range (1,n+1):
        
-        print("{}  x  {} = ".format(i,m),"{}".format(i*m))
+        print("{}  x  {} = {}".format(i,m, i*m))
     
 if __name__=="__main__":
     tables(5,10)
