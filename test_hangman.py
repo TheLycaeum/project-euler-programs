72d9f7036a390da97bf1943edd1ee1e0aad838b4
Noufal Ibrahim <noufal@nibrahim.net.in>
2016-12-10 11:21:27 +0530

    Uses tempfile instead of hardcoded name

--- test_hangman.py
+++ test_hangman.py
@@ -1,11 +1,16 @@
 from hangman import secret_word,replace_blanks
-def test_secret_word():
-   fname = "/home/saheed/project-euler-programs/text"
+
+import tempfile, os
+
+def test_secret_word(tmpdir):
+   print (tmpdir)
+   fname = tempfile.mktemp()
    with open(fname, "w") as f:
       for i in ["aa", "bbb", "cccccc"]:
          f.write(i+"\n")
    word = secret_word(fname, 5)
    assert word=="cccccc","Wrong word was choosen {}".format(word)  
+   os.unlink(fname)
   
    
 def test_replace_blanks():
